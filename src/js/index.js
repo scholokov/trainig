d361f6af00d195f3e0912c263c13b0897a9f7b11
Denis Scholokov <scholokov@users.noreply.github.com>
2018-12-02 23:45:42 +0200

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,33 @@
 // index.js
 import '../sass/main.scss';
+
+
+//require('bootstrap');
+function Product(name, price, picture) {
+    this.name = name;
+    this.price = price;
+    this.picture = picture;
+}
+
+var myEagle = new Product("Eagle", 999, "Eagle.png");
+var myNissan = new Product("Nissan", 888, "Nissan");
+var myMazda = new Product("Mazda", 777, "Mazda");
+
+myNissan.name = `
+dd
+ddf dddd ddddd de
+bugge d
+`;
+
+
+console.log( myNissan.name);
+
+
+import $ from 'jquery';
+
+// export for others scripts to use
+window.$ = window.jQuery = $;
+
+$(document).ready(function(){
+   console.log("Hello jQuery!");
+});
